adc020681b9af2237073051f08c493ba2d5e4580
Smit Mistry <smitmistry19@gmail.com>
2023-06-04 12:55:11 +0530

    r commit

--- src/components/hero/hero.jsx
+++ src/components/hero/hero.jsx
@@ -5,10 +5,10 @@ export default function Hero(props) {
   return (
     <div className={`hero flex-col ${props.className}`}>
       <picture className='hero-img'>
-        <source media="(min-width: 69.375em)" srcSet="/images/image-profile-desktop.webp" width='445px' height='720px'/>
-        <source media="(min-width: 48em)" srcSet="/images/image-profile-tablet.webp" width='322px'height='600px' />
-        <source media="(min-width: 23.4375em)" srcSet="/images/image-profile-mobile.webp" width='174px' height='383px'/>
-        <img src="/images/image-profile-mobile.webp" alt="Image of Adam Keyes"  />
+        <source media="(min-width: 69.375em)" srcSet="./images/image-profile-desktop.webp" width='445px' height='720px'/>
+        <source media="(min-width: 48em)" srcSet="./images/image-profile-tablet.webp" width='322px'height='600px' />
+        <source media="(min-width: 23.4375em)" srcSet="./images/image-profile-mobile.webp" width='174px' height='383px'/>
+        <img src="./images/image-profile-mobile.webp" alt="Image of Adam Keyes"  />
       </picture>
       <div className="hero-txt">
         <h1 className="hero-heading">Nice to <br/> meet you! I&apos;m <span className='name-underline'>Adam Keyes</span>.</h1>
